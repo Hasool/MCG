974e91d451ba10276899a1210f6606d5d9588606
MekkyOT <mekkyot@gmail.com>
2024-11-24 20:45:42 +0100

    noting new

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello wrld!");
+        System.out.println("younes gay");
+        System.out.println("hsisn rajl");
     }
 }
