b0d6e22fd2e30d707fc11b0e9ff832583cdd86e2
MekkyOT <mekkyot@gmail.com>
2024-11-26 10:20:05 +0100

    lets start

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
-
-        System.out.println("im gay");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Hello!");
 
     }
 
